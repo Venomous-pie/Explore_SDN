4834688fe95a5d1c807683fbac53210e29d3a6b5
edrian090 <162600901+edrian090@users.noreply.github.com>
2025-11-02 21:12:35 +0800

    NONE

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -7,5 +7,10 @@ import Highlight from '@/components/Highlight.vue';
   <div class="h-full">
     <Carousel />
     <Highlight />
+    <div class="flex justify-center p-16">
+      <span class="text-6xl font-serif" data-aos="fade-right">
+        Experience Surigao del Norte
+      </span>
+    </div>
   </div>
 </template>
