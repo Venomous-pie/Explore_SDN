bf69d1af23b0935bc9ca1d64a8bc3d395c4b58e0
Venomous-pie <dedicatoria.work@gmail.com>
2025-11-15 00:23:54 +0800

    removed the vite error

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,32 +3,22 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import vueDevTools from 'vite-plugin-vue-devtools'
 import UnoCSS from 'unocss/vite'
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => {
-  const plugins = [
+export default defineConfig({
+  plugins: [
     vue(),
     vueJsx(),
     UnoCSS(),
-  ]
-
-  // Enable Vue DevTools only in development (when running `vite` dev server)
-  if (command === 'serve') {
-    plugins.push(vueDevTools())
-  }
-
-  return {
-    plugins,
-    resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url))
-      },
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     },
-    server: {
-      host: true,
-      port: 5173,
-    },
-  }
+  },
+  server: {
+    host: true,
+    port: 5173,
+  },
 })
